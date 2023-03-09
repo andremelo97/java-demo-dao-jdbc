9f1b34823ecc305e9dda8fa998554d49aa7e56aa
André Melo <andre.melo9715@gmail.com>
2023-03-09 16:39:07 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,6 +1,9 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,10 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
+		Seller seller = new Seller(1, "Bob Burnquist", "bob@gmail.com", new Date(), 3000.00, obj);
+		
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
